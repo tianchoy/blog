82fad2540718a23deedc41471d11e1f22175089b
tianchoy <tianchao7c@gmail.com>
2017-03-20 09:12:24 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,9 @@
 <?php
-// 连主库
-$link=mysql_connect("sqld.duapp.com".':'."4050","b8c008567f27469380aa9dcf469b96cb","2fbefac1df9a4453aa537322200a31fe");
-
-// 连从库
-// $link=mysql_connect(SAE_MYSQL_HOST_S.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
-
-if($link)
-{
-    mysql_select_db(QBvxfZVtEdNlRLcqkHtw,$link);
-    //your code goes here
-}
+$sit_link="localhost";
+$user="root";
+$pw="root";
+$sql="blog";
+@$link=mysql_connect($sit_link,$user,$pw) or die ("数据库连接失败");
+$blog=mysql_select_db($sql,$link) or die ("数据库连接失败");
+mysql_query("SET NAME 'utf-8'");
 ?>
