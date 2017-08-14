71da7a6360ef7f635de10abf93a76fb734811d96
tianchoy <tianchao7c@gmail.com>
2017-08-14 14:38:49 +0800

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+<div class="col-md-12 col-sm-12 col-xs-12 footer">© Copyright 2014-2017 All Rights Reserved  Power by <a href="./about.php" target="_blank">tianchao</a> | V1.8.7</div>
+<script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/backtop.js" type="text/javascript"></script>
+<script src="layui/layer/layer.js"></script>
+<script>
+    $(".msg").click(function(){
+        layer.open({
+            type: 2,
+            title: '私信',
+            skin: 'layui-layer-rim', //加上边框
+            area: ['30%', '35%'],
+            content: ['message.php', 'no'] //iframe的url，no代表不显示滚动条
+        });
+    })
+</script>
+<script src="https://s4.cnzz.com/z_stat.php?id=1261532321&web_id=1261532321" language="JavaScript"></script>
+</body>
+</html>
